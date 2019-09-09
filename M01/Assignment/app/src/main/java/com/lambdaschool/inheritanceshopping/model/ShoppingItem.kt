e101863b4b96006c2ad1ef18af5b362a26559ce4
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-09 19:04:12 -0400

    built out data model

--- M01/Assignment/app/src/main/java/com/lambdaschool/inheritanceshopping/model/ShoppingItem.kt
+++ M01/Assignment/app/src/main/java/com/lambdaschool/inheritanceshopping/model/ShoppingItem.kt
@@ -2,10 +2,53 @@ package com.lambdaschool.inheritanceshopping.model
 
 import java.io.Serializable
 
-class ShoppingItem (
+open class ShoppingItem
+
+    protected constructor (
     val colorId: Int,
     val productName: String
-)
+
+) {
+
+    open fun getDisplayName(): String {
+        return productName
+    }
+
+}
+
+class GroceryItem(colorId: Int, productName: String, val needsRefrigeration: Boolean): ShoppingItem(colorId, productName) {
+    override fun getDisplayName(): String {
+
+        var refrigeration = ""
+        if (needsRefrigeration) {
+            refrigeration = "Chill It!"
+        } else {
+            refrigeration = "Pantry It!"
+        }
+
+        return "Product Name: $productName\nStorage Location: $refrigeration"
+    }
+}
+
+class ClothingItem(colorId: Int, productName: String, val waistSize: String): ShoppingItem(colorId, productName) {
+    override fun getDisplayName(): String {
+        return "Product Name: $productName\nWaist Size: $waistSize"
+    }
+}
+
+class ElectronicsItem(colorId: Int, productName: String, val onSale: Boolean): ShoppingItem(colorId, productName) {
+    override fun getDisplayName(): String {
+
+        var itemOnSale = ""
+        if (onSale) {
+            itemOnSale = "Yup! Don't resist..."
+        } else {
+            itemOnSale = "Nope! Resist..."
+        }
+
+        return "Product Name: $productName\nItem Sale: $itemOnSale"
+    }
+}
 
 object ShoppingListItem {
     val shoppingListItem = mutableListOf<ShoppingItem>()
